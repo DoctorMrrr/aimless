f456e1c32d1634206db00f4c92de4fa2b6267875
Maryna <doctormrrr@ukr.net>
2014-12-19 18:39:02 +0200

    Полагодили анімацію ура!

--- Assets/flight.cs
+++ Assets/flight.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class flight : MonoBehaviour {
-	
+public class flight : MonoBehaviour
+{
 	public float flightSpeed;
 	public float rotation_factor = 1f;
 	public Direction direction;
 	public Animator animator;
 	public bool startengine = false;
+
 	// Use this for initialization
 	void Start () {
 
@@ -17,36 +18,24 @@ public class flight : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		rigidbody2D.AddForce ((new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"))*flightSpeed ));
-		
-		direction.vector = rigidbody2D.velocity;
-		if (direction.vector.magnitude > 0) 
+		direction.vector = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")).normalized;
+		if ( direction.vector.sqrMagnitude > 0.1f )
 		{
-						float angle = rigidbody2D.rotation;
-
-						float difference =
+			rigidbody2D.AddForce (direction.vector * flightSpeed);
+			animator.SetBool ("engine", true);
+			float angle = rigidbody2D.rotation;
+			float difference =
 				Mathf.Rad2Deg * Mathf.Atan2 (
 					Mathf.Sin (Mathf.Deg2Rad * direction.angle - Mathf.Deg2Rad * angle),
 					Mathf.Cos (Mathf.Deg2Rad * direction.angle - Mathf.Deg2Rad * angle)
-						);
-						rigidbody2D.angularVelocity = difference * rotation_factor;
-				}
+					);
+			rigidbody2D.angularVelocity = difference * rotation_factor;
+		}
+		else
+		{
+			animator.SetBool ("engine", false);
+		}
 		Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
-
-		if(animator)
-		{
-
-			if  (Input.GetButtonDown("Horizontal"))
-		{
-
-			animator.SetBool("startengine", true );
-
-				
-		}
-		else {
-			animator.SetBool("startengine", false );
-		}
-		}
 	}
 }
 	
